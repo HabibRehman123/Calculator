eaa1bc73763f36667730fe8fe8ba4910566800d5
Habib Rehman <40871363+HabibRehman123@users.noreply.github.com>
2020-05-26 21:57:53 -0400

    updated for user object and api call

--- src/App.jsx
+++ src/App.jsx
@@ -8,25 +8,65 @@ import * as math from "mathjs";
 class App extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
-      input: ""
+      //route: "signin",
+      //isSignedIn: false,
+      user: {
+        id:'' ,
+        name:'' ,
+        email:'' ,
+        input: ''
+      }
     };
   }
 
+  // loadUser = (data) => {
+  //   this.setState({user: {
+  //     id: data.id,
+  //     name: data.name,
+  //     email: data.email,
+  //     input: data.input
+  //   }})
+  //   console.log('User previous value is ', this.state.user.input)
+  // }
+
+
   addToInput = val => {
-    this.setState({ input: this.state.input + val });
+    var user = {...this.state.user}
+    user.input=user.input+val
+    this.setState({user})
   };
 
   handleEqual = () => {
-    this.setState({ input: math.evaluate(this.state.input) });
+    var user = {...this.state.user}
+    user.input = math.evaluate(user.input)
+    this.setState({user})
+    fetch('http://localhost:5000/update', {
+      method: 'post',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        //id:this.state.user.id,
+        input: this.state.user.input,
+      })
+    })
+    .then(response => response.json())
+    .then(response => {
+      console.log(response)
+    })
   };
-
+  
+  handleClear = () => {
+    var user = {...this.state.user}
+    user.input = ""
+    this.setState({user})
+  }
+  
   render() {
     return (
       <div className="App">
+
         <div className="calc-wrapper">
-          <Input input={this.state.input} />
+          <Input input={this.state.user.input} />
           <div className="row">
             <Button handleClick={this.addToInput}>7</Button>
             <Button handleClick={this.addToInput}>8</Button>
@@ -48,11 +88,11 @@ class App extends Component {
           <div className="row">
             <Button handleClick={this.addToInput}>.</Button>
             <Button handleClick={this.addToInput}>0</Button>
-            <Button handleClick={() => this.handleEqual()}>=</Button>
+            <Button handleClick={this.handleEqual}>=</Button>
             <Button handleClick={this.addToInput}>-</Button>
           </div>
           <div className="row">
-            <ClearButton handleClear={() => this.setState({ input: "" })}>
+            <ClearButton handleClear={this.handleClear}>
               Clear
             </ClearButton>
           </div>
--- src/components/ClearButton.jsx
+++ src/components/ClearButton.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import "./ClearButton.css";
 
 export const ClearButton = props => (
-  <div className="clear-btn" onClick={props.handleClear}>
+  <div className="clear-btn" onClick={() => props.handleClear()}>
     {props.children}
   </div>
 );
+
